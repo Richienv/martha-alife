2b62c45ecec7f006c0b2718d54d7d260bbff9507
Richienv <kidnovell.richie@gmail.com>
2025-08-02 07:04:09 +0800

    Remove auth pages, blog pages, and contact page

--- src/app/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ContactForm } from '@/components/contact-form';
-
-export default function ContactPage() {
-  return (
-    <div className="w-full">
-      <ContactForm />
-    </div>
-  );
-}
